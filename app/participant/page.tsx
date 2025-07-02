04feb0a9429f21ee9fd2ed81638a76d45e494f19
ANNIE\annau <anna.ukrizu@gmail.com>
2025-07-02 18:46:03 +0200

    Participant page

--- app/participant/page.tsx
+++ app/participant/page.tsx
@@ -9,12 +9,12 @@ export default function ParticipantPage() {
 
       <section>
         <h2 className="participant-subtitle">Personal Information</h2>
-        <div className="form-field">Luboš Kříž</div>
-        <div className="form-field">ukrizu@gmail.com</div>
-        <div className="form-field">+420739656314</div>
-        <div className="form-field">Lesní 729</div>
-        <div className="form-field">17/10/1976</div>
-        <div className="form-field">M</div>
+        <label htmlFor="">Name:</label><div className="form-field">Luboš Kříž</div>
+        <label htmlFor="">Email:</label><div className="form-field">ukrizu@gmail.com</div>
+        <label htmlFor="">Phone:</label><div className="form-field">+420739656314</div>
+        <label htmlFor="">Address:</label><div className="form-field">Lesní 729</div>
+        <label htmlFor="">Date of birth:</label><div className="form-field">17/10/1976</div>
+        <label htmlFor="">Gender:</label><div className="form-field">M</div>
       </section>
 
       <section>
@@ -32,43 +32,47 @@ export default function ParticipantPage() {
 
       <section>
         <h2 className="participant-subtitle">Holotropic Breathwork Sessions</h2>
-        <div className="form-field">Single - 1/2017 - Nesmen, Czech Republic - Ludmila Chrášťanská, Jaroslav Kursa, Jan Šilar</div>
-        <div className="form-field">Double - 4/2017 - Nesmen, Czech Republic - Ludmila Chrášťanská, Jaroslav Kursa, Jan Šilar</div>
-        <div className="form-field">Single - 10/2017 - Prague, Czech Republic - Jan Šilar</div>
-        <div className="form-field">Single - 1/2018 - Nesmen, Czech Republic - Ludmila Chrášťanská, Jaroslav Kursa, Petr Pacovsky</div>
-        <div className="form-field">Double - 5/2018 - Nesmen, Czech Republic - Ludmila Chrášťanská, Jaroslav Kursa, Jan Šilar</div>
-        <div className="form-field">Double - 9/2018 - Holos Vlaštovičky, Czech Republic - Milan Hrabánek, Svatava Bardynová</div>
-        <div className="form-field">Single - 12/2018 - Nesmen, Czech Republic - Ludmila Chrášťanská, Jaroslav Kursa, Karel Fuller</div>
-        <div className="form-field">Single - 3/2019 - Prague, Czech Republic - Jan Šilar</div>
-        <div className="form-field">Single - 1/2020 - Nesmen, Czech Republic - Ludmila Chrášťanská, Jaroslav Kursa, Jan Šilar</div>
-        <div className="form-field">Single - 3/2024 - Prague, Czech Republic - Roman Petrov, Andrea Košiarová</div>
-        <div className="form-field">Double - 6/2024 - Nesmen, Czech Republic - Jaroslav Kursa, Jan Šilar</div>
+        <div className="form-field row">Single - 1/2017 - Nesmen, Czech Republic - Ludmila Chrášťanská, Jaroslav Kursa, Jan Šilar <FaTrash className="icon" /></div>
+        <div className="form-field row">Double - 4/2017 - Nesmen, Czech Republic - Ludmila Chrášťanská, Jaroslav Kursa, Jan Šilar <FaTrash className="icon" /></div>
+        <div className="form-field row">Single - 10/2017 - Prague, Czech Republic - Jan Šilar <FaTrash className="icon" /></div>
+        <div className="form-field row">Single - 1/2018 - Nesmen, Czech Republic - Ludmila Chrášťanská, Jaroslav Kursa, Petr Pacovsky <FaTrash className="icon" /></div>
+        <div className="form-field row">Double - 5/2018 - Nesmen, Czech Republic - Ludmila Chrášťanská, Jaroslav Kursa, Jan Šilar <FaTrash className="icon" /></div>
+        <div className="form-field row">Double - 9/2018 - Holos Vlaštovičky, Czech Republic - Milan Hrabánek, Svatava Bardynová <FaTrash className="icon" /></div>
+        <div className="form-field row">Single - 12/2018 - Nesmen, Czech Republic - Ludmila Chrášťanská, Jaroslav Kursa, Karel Fuller <FaTrash className="icon" /></div>
+        <div className="form-field row">Single - 3/2019 - Prague, Czech Republic - Jan Šilar <FaTrash className="icon" /></div>
+        <div className="form-field row">Single - 1/2020 - Nesmen, Czech Republic - Ludmila Chrášťanská, Jaroslav Kursa, Jan Šilar <FaTrash className="icon" /></div>
+        <div className="form-field row">Single - 3/2024 - Prague, Czech Republic - Roman Petrov, Andrea Košiarová <FaTrash className="icon" /></div>
+        <div className="form-field row">Double - 6/2024 - Nesmen, Czech Republic - Jaroslav Kursa, Jan Šilar <FaTrash className="icon" /></div>
+        <button className="add-button"><span className="add-icon"><FaPlus /></span><span className="add-text">Add Session</span></button>
       </section>
 
       <section>
         <h2 className="participant-subtitle">Apprenticing Experience</h2>
-        <div className="form-field">Single - 1/2025 - Prague, Czech Republic - Pavel Ševčík, Lucie Heralová</div>
-        <div className="form-field">Single - 2/2025 - Brno, Czech Republic - Magda Šimečková, Lukáš Srbecký</div>
-        <div className="form-field">Double - 3/2025 - Brno, Czech Republic - Magda Šimečková, Lukáš Srbecký</div>
-        <div className="form-field">Double - 3/2025 - GTT Module: Holotropic Paradigm, Austria (Sitara, Peter, Thomas)</div>
+        <div className="form-field row">Single - 1/2025 - Prague, Czech Republic - Pavel Ševčík, Lucie Heralová <FaTrash className="icon" /></div>
+        <div className="form-field row">Single - 2/2025 - Brno, Czech Republic - Magda Šimečková, Lukáš Srbecký <FaTrash className="icon" /></div>
+        <div className="form-field row">Double - 3/2025 - Brno, Czech Republic - Magda Šimečková, Lukáš Srbecký <FaTrash className="icon" /></div>
+        <div className="form-field row">Double - 3/2025 - GTT Module: Holotropic Paradigm, Austria (Sitara, Peter, Thomas) <FaTrash className="icon" /></div>
+        <button className="add-button"><span className="add-icon"><FaPlus /></span><span className="add-text">Add Experience</span></button>
       </section>
 
       <section>
         <h2 className="participant-subtitle">Individual Consultations</h2>
-        <div className="form-field">26/2/2024 Peter Köllerer</div>
-        <div className="form-field">11/3/2024 Roman Petrov</div>
-        <div className="form-field">16/5/2024 Sonja Busch</div>
-        <div className="form-field">13/6/2024 Sitara Blasco</div>
-        <div className="form-field">24/6/2024 Sitara Blasco</div>
-        <div className="form-field">29/7/2024 Thomas Liska</div>
-        <div className="form-field">17/12/2024 Sonja Busch</div>
-        <div className="form-field">1/2/2025 Lukas Srbecky</div>
-        <div className="form-field">2/2/2025 Magdalena Simeckova</div>
+        <div className="form-field row">26/2/2024 Peter Köllerer <FaTrash className="icon" /></div>
+        <div className="form-field row">11/3/2024 Roman Petrov <FaTrash className="icon" /></div>
+        <div className="form-field row">16/5/2024 Sonja Busch <FaTrash className="icon" /></div>
+        <div className="form-field row">13/6/2024 Sitara Blasco <FaTrash className="icon" /></div>
+        <div className="form-field row">24/6/2024 Sitara Blasco <FaTrash className="icon" /></div>
+        <div className="form-field row">29/7/2024 Thomas Liska <FaTrash className="icon" /></div>
+        <div className="form-field row">17/12/2024 Sonja Busch <FaTrash className="icon" /></div>
+        <div className="form-field row">1/2/2025 Lukas Srbecky <FaTrash className="icon" /></div>
+        <div className="form-field row">2/2/2025 Magdalena Simeckova <FaTrash className="icon" /></div>
+        <button className="add-button"><span className="add-icon"><FaPlus /></span><span className="add-text">Add Consultation</span></button>
       </section>
 
       <section>
         <h2 className="participant-subtitle">Recommendation</h2>
-        <div className="form-field">Sonja Busch</div>
+        <div className="form-field row">Sonja Busch <FaTrash className="icon" /></div>
+        <button className="add-button"><span className="add-icon"><FaPlus /></span><span className="add-text">Add Recommendation</span></button>
       </section>
     </main>
   );
